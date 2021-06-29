411589d3951bdb9419883a933eb0457b0c1b748a
Eugene M <darkstarmacho@gmail.com>
2021-06-29 09:44:31 +0300

    Initialize renderer, tuxtures and buffer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <SDL.h>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 const int SCREEN_WIDTH = 800;
-const int SCREE_HEIGHT = 600;
+const int SCREEN_HEIGHT = 600;
 
 bool isRunning = true;
 
@@ -19,7 +20,7 @@ int main(int argc, char *argv[]) {
         SDL_WINDOWPOS_UNDEFINED, 
         SDL_WINDOWPOS_UNDEFINED,
         SCREEN_WIDTH,
-        SCREE_HEIGHT,
+        SCREEN_HEIGHT,
         SDL_WINDOW_SHOWN);
 
     if(window == NULL) {
@@ -27,6 +28,37 @@ int main(int argc, char *argv[]) {
         SDL_Quit();
         return 1;
     }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer, 
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STATIC,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT);
+
+    if(renderer == NULL) {
+        cout << "Could not create renderer: " << SDL_GetError() << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    if(texture == NULL) {
+        cout << "Could not create texture: " << SDL_GetError() << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    Uint32 *buf = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    memset(buf, 255, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+    SDL_UpdateTexture(texture, NULL, buf, SCREEN_WIDTH * sizeof(Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
     SDL_Event event;
 
@@ -45,8 +77,10 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    delete[] buf;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
-    cout << "Windows destroyed" << endl;
     SDL_Quit();
 
     return 0;
